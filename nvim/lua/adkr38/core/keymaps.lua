53fe8a94577d3eb31ee846732a8b0ec71758acbb
adriankr38 <adrian58ago@gmail.com>
2022-12-11 18:46:37 +0100

    update preferences on <code>keymaps.lua<code>

--- nvim/lua/adkr38/core/keymaps.lua
+++ nvim/lua/adkr38/core/keymaps.lua
@@ -6,6 +6,9 @@ local keymap = vim.keymap
 keymap.set("n", "<C-d>", "<C-d>zz") -- Down
 keymap.set("n", "<C-u>", "<C-u>zz") -- Up
 
+-- New tab with terminal
+keymap.set("n", "<leader>tt", ":tabnew<CR>:term<CR>")
+
 -- Delete character in NORMAL mode & don't save to register with x
 keymap.set("n", "x", '"_x')
 -- Increment | Decrement numbers in NORMAL mode with +-
@@ -25,7 +28,7 @@ keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & heig
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
 keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
-keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>te", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
@@ -54,5 +57,4 @@ keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current c
 keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
 
 --zen mode
-
 keymap.set("n", "<leader>zm", "<cmd>ZenMode<cr>", { silent = true })
