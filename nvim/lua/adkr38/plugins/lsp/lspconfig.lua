36e30d956d1bcc603808ab3f4752f63f776f45ee
adriankr38 <adrian58ago@gmail.com>
2023-02-28 22:11:07 +0000

    fix(lua): lspsaga conflicts

--- nvim/lua/adkr38/plugins/lsp/lspconfig.lua
+++ nvim/lua/adkr38/plugins/lsp/lspconfig.lua
@@ -16,6 +16,7 @@ if not typescript_setup then
 	return
 end
 
+require("lspsaga")
 local keymap = vim.keymap
 
 local on_attach = function(client, bufnr)
@@ -91,7 +92,7 @@ lspconfig["tsserver"].setup({
 	on_attach = on_attach,
 })
 -- configure lua server (with special settings)
-lspconfig["sumneko_lua"].setup({
+lspconfig["lua_ls"].setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
 	settings = { -- custom settings for lua
--- nvim/lua/adkr38/plugins/lsp/lspsaga.lua
+++ nvim/lua/adkr38/plugins/lsp/lspsaga.lua
@@ -1,20 +1,14 @@
-
-
--- import lspsaga safely
-local saga_status, saga = pcall(require, "lspsaga")
-if not saga_status then
-  return
+local status, saga = pcall(require, "lspsaga")
+if not status then
+	return
 end
 
-saga.init_lsp_saga({
-  -- keybinds for navigation in lspsaga window
-  move_in_saga = { prev = "<C-p>", next = "<C-n>" },
-  -- use enter to open file with finder
-  finder_action_keys = {
-    open = "<CR>",
-  },
-  -- use enter to open file with definition preview
-  definition_action_keys = {
-    edit = "<CR>",
-  },
+saga.setup({
+	diagnostics = {
+		on_insert = false,
+		on_insert_follow = true,
+	},
+	symbol_in_winbar = {
+		separator = "  ",
+	},
 })
