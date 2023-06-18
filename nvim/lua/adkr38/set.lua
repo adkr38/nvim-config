b8cae41fb6fd5634049bd7701cba6df5eb51fd30
adriankr38 <adrian58ago@gmail.com>
2023-06-18 15:05:20 +0200

    add(lua): Yank highlighting

--- nvim/lua/adkr38/set.lua
+++ nvim/lua/adkr38/set.lua
@@ -39,3 +39,36 @@ vim.opt.guicursor = "n:-blinkon100-blinkoff200"
 -- Other
 vim.opt.updatetime = 50
 vim.g.mapleader = " "
+
+--yank highlighting
+
+local augroup = vim.api.nvim_create_augroup
+local HighlightGroup = augroup('adkr38', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = HighlightGroup,
+    pattern = "*",
+    command = "%s/\\s\\+$//e",
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
